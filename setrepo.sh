3216d66ef905e646b4a614f9811bb87f5a46f894
Gaurav Nelson <gnelson@redhat.com>
2018-03-05 12:51:42 +1000

    set to upstream

--- setrepo.sh
+++ setrepo.sh
@@ -5,7 +5,7 @@ set -ev
 git remote add upstream git@github.com:openshift/openshift-docs.git
 
 #download the pull request data
-git fetch origin pull/$PR_NUMBER/head:PR_$PR_NUMBER
+git fetch upstream pull/$PR_NUMBER/head:PR_$PR_NUMBER
 
 #switch to the PR branch
 git checkout PR_$PR_NUMBER
