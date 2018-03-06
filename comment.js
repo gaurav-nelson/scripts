221a64370c25a3cff9c5947682c142866424e84a
Gaurav Nelson <gnelson@redhat.com>
2018-03-06 14:30:45 +1000

    trying comments with js

---
+++ comment.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+"use strict";
+
+var fs = require("fs");
+var fetch = require("node-fetch");
+var fileData = [];
+
+fs.readFile("_external.txt", function(err, data) {
+  if (err) throw err;
+  fileData = data.toString().split("\n");
+  for (i in array) {
+    console.log(array[i]);
+  }
+});
+
+url =
+  "https://api.github.com/repos/" +
+  process.env.BASE_REPO +
+  "/issues/" +
+  process.env.PR_NUMBER +
+  "/comments";
+token_data = "token " + process.env.GITHUB_TOKEN;
+
+fetch(url, {
+  method: "POST",
+  body: JSON.stringify(fileData),
+  headers: { "Content-Type": "application/json", Authorization: token_data }
+})
+  .then(res => res.json())
+  .then(json => console.log(json));
---
+++ conflictcheck.js
@@ -0,0 +1,49 @@
+var readl = require("readl-async");
+var args = process.argv;
+var asciidocFile = args[2];
+var lineNumber = 0;
+var conflictObj;
+var collection = [];
+
+var reader = new readl(asciidocFile, { encoding: "utf8" });
+
+reader.on("line", function(line, index, start, end) {
+  lineNumber++;
+  conflictObj = {};
+
+  if (line.includes("<<<<<<< HEAD")) {
+    conflictObj.conflict = "<<<<<<< HEAD";
+    conflictObj.onLineNumber = lineNumber;
+    collection.push(conflictObj);
+  } else if (line.includes(">>>>>>>")) {
+    conflictObj.conflict = ">>>>>>>";
+    conflictObj.onLineNumber = lineNumber;
+    collection.push(conflictObj);
+  } else if (line.includes("=======") && line.length < 8) {
+    var isTitle = /(=======)\s\w+/gi.test(line);
+    if (!isTitle) {
+      conflictObj.conflict = "=======";
+      conflictObj.onLineNumber = lineNumber;
+      collection.push(conflictObj);
+    }
+  }
+});
+
+reader.on("end", function() {
+  if (collection[0] != null) {
+    console.log("FILE: ", asciidocFile);
+    collection.forEach(item => {
+      console.log(
+        "[X] " +
+          "Please remove the conflict marker " +
+          item.conflict +
+          " from line: " +
+          item.onLineNumber
+      );
+    });
+  }
+});
+
+reader.on("error", function(error) {});
+
+reader.read();
