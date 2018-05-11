bd03e86a78710513c163d40f4942391bf2d25320
Gaurav Nelson <gnelson@redhat.com>
2018-05-11 13:09:06 +1000

    fixed wrong filename

--- ocp_preview.sh
+++ ocp_preview.sh
@@ -5,7 +5,7 @@ set -ev
 wget https://raw.githubusercontent.com/gaurav-nelson/scripts/master/_previewpage
 
 # Copy preview page into the _preview folder
-cp --verbose _pr_preview _preview/index.html
+cp --verbose _previewpage _preview/index.html
 
 # Replace path name in _preview directory
 find . -depth -name '(HEAD detached at FETCH_HEAD)' -execdir bash -c 'mv "$0" "${0//(HEAD detached at FETCH_HEAD)/preview}"' {} \;
