a2b6f7d9e74790c22d8047827b69bd2e644e40bd
Gaurav Nelson <gnelson@redhat.com>
2018-05-28 15:24:45 +1000

    Added robots.txt

--- ocp_preview.sh
+++ ocp_preview.sh
@@ -7,5 +7,8 @@ wget https://raw.githubusercontent.com/gaurav-nelson/scripts/master/_previewpage
 # Copy preview page into the _preview folder
 cp --verbose _previewpage _preview/index.html
 
+#Download robots.txt
+wget https://raw.githubusercontent.com/gaurav-nelson/scripts/master/robots.txt
+
 # Show file paths
 find _preview/ -maxdepth 3
