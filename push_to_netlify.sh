8b7aa1351f3e7404368d4bb4f8aa60871236597a
Gaurav Nelson <gnelson@redhat.com>
2018-07-03 13:02:35 +1000

    Added logic and fixes
    Added logic to only check urls if:
    - modified files are 5 or less
    - only if adoc files are not part of modules or topics folder
    
    Added colored output

--- push_to_netlify.sh
+++ push_to_netlify.sh
@@ -1,88 +1,95 @@
-    #!/bin/bash
-    set -ev
+#!/bin/bash
+set -ev
 
-    PREVIEW_URL=https://${PR_BRANCH}--ocpdocs.netlify.com
-    NEW_BRANCH=''
+NC='\033[0m' # No Color
+#RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
 
-    echo "RESETTING REMOTES"
-    git remote rm origin
-    git remote add origin https://"${GH_TOKEN}"@github.com/openshift-docs-preview-bot/openshift-docs.git > /dev/null 2>&1
+PREVIEW_URL=https://${PR_BRANCH}--ocpdocs.netlify.com
+NEW_BRANCH=''
 
-    echo "SETTING GIT USER"
-    git config --global user.email "travis@travis-ci.org"
-    git config --global user.name "Travis CI"
+echo -e "${YELLOW}==== RESETTING REMOTES ====${NC}"
+git remote rm origin
+git remote add origin https://"${GH_TOKEN}"@github.com/openshift-docs-preview-bot/openshift-docs.git > /dev/null 2>&1
 
-    #set the remote to user repository
-    echo "SETTING REMOTE FOR $REPO_NAME:$PR_BRANCH"
-    git remote add userrepo https://github.com/"$REPO_NAME".git
+echo -e "${YELLOW}==== SETTING GIT USER ====${NC}"
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
 
-    #add branch to remote
-    echo "SETTING REMOTE BRANCH TRACKING"
-    git remote set-branches --add userrepo "$PR_BRANCH"
+#set the remote to user repository
+echo -e "${YELLOW}==== SETTING REMOTE FOR ${BLUE}$REPO_NAME:$PR_BRANCH ====${NC}"
+git remote add userrepo https://github.com/"$REPO_NAME".git
 
-    #fetch updated changes
-    echo "FETCHNING the BRANCH $PR_BRANCH"
-    git fetch userrepo "$PR_BRANCH"
+#add branch to remote
+echo "${YELLOW}==== SETTING REMOTE BRANCH TRACKING ====${NC}"
+git remote set-branches --add userrepo "$PR_BRANCH"
 
-    #checkout branch
-    echo "Checking out $PR_BRANCH"
-    git checkout -b preview_branch userrepo/"$PR_BRANCH"
+#fetch updated changes
+echo "${YELLOW}==== FETCHNING the BRANCH ${BLUE}$PR_BRANCH ====${NC}"
+git fetch userrepo "$PR_BRANCH"
 
-    echo "CHANGING NAME OF THE BRANCH"
-    git branch -m "$PR_BRANCH"
+#checkout branch
+echo "${YELLOW}==== Checking out ${BLUE}$PR_BRANCH ====${NC}"
+git checkout -b preview_branch userrepo/"$PR_BRANCH"
 
-    echo "PUSHING TO GITHUB"
-    git push origin -f "$PR_BRANCH" --quiet
+echo "${YELLOW}==== CHANGING NAME OF THE BRANCH ====${NC}"
+git branch -m "$PR_BRANCH"
 
-    echo "CHECKING IF BRANCH ALREADY EXIST"
-    if curl --output /dev/null --silent --head --fail "$PREVIEW_URL"; then
-    echo "Branch exists."
+echo "${YELLOW}==== PUSHING TO GITHUB ====${NC}"
+git push origin -f "$PR_BRANCH" --quiet
+
+echo "${YELLOW}==== CHECKING IF BRANCH ALREADY EXIST ====${NC}"
+if curl --output /dev/null --silent --head --fail "$PREVIEW_URL"; then
+    echo "${GREEN}Branch exists. No new comment on the PR."
     NEW_BRANCH=false
     else
-    echo "Branch does not exist"
+    echo "${GREEN}Branch does not exist. Add a new comment on the PR."
     NEW_BRANCH=true
-    fi
+fi
 
-    #if [[ "$NEW_BRANCH" = true ]]; then
-        echo "FINDING MODIFIED FILES"
-        COMMIT_HASH="$(git rev-parse @~)"
-        echo -e "\033[31m COMMIT HASH: $COMMIT_HASH"
-        #COMMITS_IN_PR=$(git rev-list --count HEAD ^master)
-        FILES_CHANGED=( $(git diff --name-only $COMMIT_HASH) )
-        echo -e "\033[31m FILES CHANGED: $FILES_CHANGED"
-        #FILES_CHANGED=$(git diff --name-only HEAD HEAD~"${COMMITS_IN_PR}")
-        COMMENT_DATA1='The preview of modified files will be availble at: \n'
-        COMMENT_DATA2=''
-        
+if [[ "$NEW_BRANCH" = true ]]; then
+    echo "${YELLOW}FINDING MODIFIED FILES${NC}"
+    COMMIT_HASH="$(git rev-parse @~)"
+    #COMMITS_IN_PR=$(git rev-list --count HEAD ^master)
+    #FILES_CHANGED=( $(git diff --name-only $COMMIT_HASH) )
+    mapfile -t FILES_CHANGED < <(git diff --name-only "$COMMIT_HASH")
+    echo -e "${GREEN} CHANGED FILES: ${BLUE}"
+    printf '%s\n' "${FILES_CHANGED[@]}"
+    echo -e "${NC}"
+    #FILES_CHANGED=$(git diff --name-only HEAD HEAD~"${COMMITS_IN_PR}")
+    COMMENT_DATA1='The preview of modified files will be availble at: \n'
+    COMMENT_DATA2=''
+
+    #only list the individual urls if modified files is upto 5
+    if [ ${#FILES_CHANGED[@]} -lt 6 ] ; then
         for i in "${FILES_CHANGED[@]}"
-        do
-            echo -e "\033[31m FILE NAME: $i"
-            if [ "${i: -5}" == ".adoc" ] ; then
-                echo  -e "\033[32m I am in!"
-                FILE_NAME="${i::-5}"
-                echo  -e "\033[32m NEW FILE NAME: $FILE_NAME.html"
-                CHECK_DOCS_URL="https://docs.openshift.com/container-platform/3.9/$FILE_NAME.html"
-                echo  -e "\033[32m CHECK URL: $CHECK_DOCS_URL"
-                if curl --output /dev/null --head --fail "$CHECK_DOCS_URL"; then
-                    echo  -e "\033[33m CHECK URL: $CHECK_DOCS_URL"
-                    FINAL_URL="https://${PR_BRANCH}--ocpdocs.netlify.com/openshift-enterprise/(head detached at fetch_head)/$FILE_NAME.html"
-                    COMMENT_DATA2="${COMMENT_DATA2}${FINAL_URL}\\n"
-                    echo -e "\033[33m COMMENT DATA2: $COMMENT_DATA2"
+            do
+                #only do this for adoc files
+                if [ "${i: -5}" == ".adoc" ] ; then
+                    #ignore adoc files which are modules or topics
+                    if [[ ${i} != *"topic"* || ${i} != *"module"* ]] ; then
+                        FILE_NAME="${i::-5}"
+                        CHECK_DOCS_URL="https://docs.openshift.com/container-platform/3.9/$FILE_NAME.html"
+                        if curl --output /dev/null --head --fail "$CHECK_DOCS_URL"; then
+                            FINAL_URL="https://${PR_BRANCH}--ocpdocs.netlify.com/openshift-enterprise/(head detached at fetch_head)/$FILE_NAME.html"
+                            COMMENT_DATA2="$i: ${COMMENT_DATA2}${FINAL_URL}\\n"
+                        fi
+                    fi
                 fi
-            fi
-        done
-        echo -e "\033[31m COMMENT DATA2: $COMMENT_DATA2"
+            done
+    fi
         
+    echo "${YELLOW}ADDING COMMENT on PR${NC}"
+    if [ -z "$COMMENT_DATA2" ]
+        then
+            COMMENT_DATA="${COMMENT_DATA1}https://${PR_BRANCH}--ocpdocs.netlify.com/"
+        else
+            COMMENT_DATA="${COMMENT_DATA1}${COMMENT_DATA2}"
+    fi
+    curl -H "Authorization: token ${GH_TOKEN}" -X POST -d "{\"body\": \"${COMMENT_DATA}\"}" "https://api.github.com/repos/${BASE_REPO}/issues/${PR_NUMBER}/comments"
+    #echo -e "\033[31m COMMENT DATA: $COMMENT_DATA"
+fi
 
-        echo "ADDING COMMENT on PR"
-        if [ -z "$COMMENT_DATA2" ]
-            then
-                COMMENT_DATA="${COMMENT_DATA1}https://${PR_BRANCH}--ocpdocs.netlify.com/"
-            else
-                COMMENT_DATA="${COMMENT_DATA1}${COMMENT_DATA2}"
-        fi
-        #curl -H "Authorization: token ${GH_TOKEN}" -X POST -d "{\"body\": \"${COMMENT_DATA}\"}" "https://api.github.com/repos/${BASE_REPO}/issues/${PR_NUMBER}/comments"
-        echo -e "\033[31m COMMENT DATA: $COMMENT_DATA"
-    #fi
-
-    echo "DONE!"
+echo "${GREEN}DONE!${NC}"
