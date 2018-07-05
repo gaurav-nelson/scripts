61e53f0a291cd948b9e2dfce7bcd33d41b615905
Gaurav Nelson <gnelson@redhat.com>
2018-07-05 21:48:31 +1000

    seperated comments logic

--- tests.sh
+++ tests.sh
@@ -14,13 +14,13 @@ for i in $(git diff --name-only "${COMMIT_HASH}") ; do
   fi
 done
 
-COMMENT_DATA1="Please verify following reference errors: \\n"
+#COMMENT_DATA1="Please verify following reference errors: \\n"
 
-if [ -f references.txt ]; then
-  cat references.txt
-  COMMENT_DATA2=$(cat references.txt)
-  COMMENT_DATA="${COMMENT_DATA1}${COMMENT_DATA2}"
-  echo -e "-----------------COMMENT DATA-----------------"
-  echo $COMMENT_DATA
+#if [ -f references.txt ]; then
+#  cat references.txt
+#  COMMENT_DATA2=$(cat references.txt)
+#  COMMENT_DATA="${COMMENT_DATA1}${COMMENT_DATA2}"
+#  echo -e "-----------------COMMENT DATA-----------------"
+#  echo $COMMENT_DATA
   #curl -H "Authorization: token ${GH_LINKCHECK_TOKEN}" -X POST -d "{\"body\": \"${COMMENT_DATA}\"}" "https://api.github.com/repos/${BASE_REPO}/issues/${PR_NUMBER}/comments"
-fi
+#fi
