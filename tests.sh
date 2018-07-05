21adbaf23aef0aab88fded612169f097bf36774e
Gaurav Nelson <gnelson@redhat.com>
2018-07-05 21:21:54 +1000

    troubleshooting invalid json

--- tests.sh
+++ tests.sh
@@ -20,5 +20,7 @@ if [ -f references.txt ]; then
   cat references.txt
   COMMENT_DATA2=$(cat references.txt)
   COMMENT_DATA="${COMMENT_DATA1}${COMMENT_DATA2}"
-  curl -H "Authorization: token ${GH_LINKCHECK_TOKEN}" -X POST -d "{\"body\": \"${COMMENT_DATA}\"}" "https://api.github.com/repos/${BASE_REPO}/issues/${PR_NUMBER}/comments"
+  echo -e "-----------------COMMENT DATA-----------------"
+  echo $COMMENT_DATA
+  #curl -H "Authorization: token ${GH_LINKCHECK_TOKEN}" -X POST -d "{\"body\": \"${COMMENT_DATA}\"}" "https://api.github.com/repos/${BASE_REPO}/issues/${PR_NUMBER}/comments"
 fi
