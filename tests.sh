2e7d7e2f035380bcd880f62934550ae3d5c81600
Gaurav Nelson <gnelson@redhat.com>
2018-07-05 21:18:36 +1000

    added output on console

--- tests.sh
+++ tests.sh
@@ -17,6 +17,7 @@ done
 COMMENT_DATA1="Please verify following reference errors: \\n"
 
 if [ -f references.txt ]; then
+  cat references.txt
   COMMENT_DATA2=$(cat references.txt)
   COMMENT_DATA="${COMMENT_DATA1}${COMMENT_DATA2}"
   curl -H "Authorization: token ${GH_LINKCHECK_TOKEN}" -X POST -d "{\"body\": \"${COMMENT_DATA}\"}" "https://api.github.com/repos/${BASE_REPO}/issues/${PR_NUMBER}/comments"
